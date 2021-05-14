711ade5deb84752ec82e80cb25de24f1b1ee5441
Vincent <qianbo@futsystems.com>
2021-05-14 21:02:03 +0800

    f

--- common/__init__.py
+++ common/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from .response import Response, Success, Error
-from .response import json_response, _json_content, _json_content_md5, _calc_md5
+from common.response import Response, Success, Error
+from common.response import json_response, _json_content, _json_content_md5, _calc_md5
