248da834052cd07e770a13a4964f9ad1dde47979
Vincent <qianbo@futsystems.com>
2021-05-14 21:00:03 +0800

    Update views.py

--- server/views.py
+++ server/views.py
@@ -21,6 +21,6 @@ def get_plot_config(request):
             ploter = Plotter.objects.get(id=id)
             plot_config = ploter.plot_config
         return json_response(plot_config.to_dict())
-    except Exception, e:
+    except Exception as e:
         logger.error(traceback.format_exc())
         return json_response(e.message)
