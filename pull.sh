a7ff2d1ed754dada220b2aee503bfa7e42dd5b6c
Vincent <qianbo@futsystems.com>
2021-06-26 13:39:32 +0800

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -6,6 +6,6 @@ SERVICE=django_chia
 
 echo -e "Restart Service"
 
-exec supervisorctl  restart django_chia
-exec supervisorctl  restart celery_beat
-exec supervisorctl  restart celery_worker
+supervisorctl  restart django_chia
+supervisorctl  restart celery_beat
+supervisorctl  restart celery_worker
