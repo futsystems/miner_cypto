93f1b778fa04d8be47492342800ec7010dde52e4
Vincent <qianbo@futsystems.com>
2021-08-02 14:20:44 +0800

    Update bsc.py

--- common/bsc.py
+++ common/bsc.py
@@ -6,15 +6,27 @@ from bscscan import BscScan
 
 
 class BSCAPI(object):
-    def __init__(self,address):
+    def __init__(self):
         self._api_key = '5B7Y6IW73Y33Z8TNPR68XD2CHP63I5CVM5'
 
-    async def async_get_balance(self, address):
+    async def async_get_bnb_balance(self, address):
         async with BscScan(self._api_key) as bsc:
             return await bsc.get_bnb_balance(address=address)
 
-    def get_balance(self, address):
+    def get_bnb_balance(self, address):
         loop = asyncio.get_event_loop()
-        task = loop.create_task(self.async_get_balance(address))
+        task = loop.create_task(self.async_get_bnb_balance(address))
         loop.run_until_complete(task)
         return task.result()
+
+    async def async_get_balance_by_token_contact(self, address, contact):
+        async with BscScan(self._api_key) as bsc:
+            return await bsc.get_acc_balance_by_token_contract_address(contract_address=address, address=address)
+
+
+    def get_token_balance(self, address, token_contact):
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(self.async_get_balance_by_token_contact(address, token_contact))
+        loop.run_until_complete(task)
+        return task.result()
+
