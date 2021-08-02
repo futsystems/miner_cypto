02ff8f3d3e1d8d0ce5b29608802423d2547c318f
Vincent <qianbo@futsystems.com>
2021-08-02 15:08:14 +0800

    add account

--- gamefi/models/account.py
+++ gamefi/models/account.py
@@ -8,7 +8,7 @@ class Account(models.Model):
     """
     Game Account
     """
-    account_id = models.CharField('ID', max_length=50, default='Game')
+    account_id = models.CharField('ID', max_length=50, default='0')
     address = models.CharField('Address', max_length=100, default='')
     chain_token_balance = models.FloatField('Chain Token', default=0)
     game_token_balance = models.FloatField('Game Token', default=0)
