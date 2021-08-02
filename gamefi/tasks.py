49eb65d44f4899e5c3a5f216ee35d678b96df0db
Vincent <qianbo@futsystems.com>
2021-08-02 15:51:28 +0800

    Update tasks.py

--- gamefi/tasks.py
+++ gamefi/tasks.py
@@ -11,12 +11,17 @@ from common.mail import send_email as _send_email
 import logging
 logger = logging.getLogger(__name__)
 
-
-
+from common.bsc import BSCAPI
+from .models import Game
 @shared_task
 def sync_account_balance():
     logger.info('sync account balance')
 
+    for game in Game.objects.all():
+        if game.chain == 'BSC':
+            api = BSCAPI()
+            for account in game.accounts.all():
+                logger.info('account address:%s' % account.address)
 
 
 
