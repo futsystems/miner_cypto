d98760154ad9a3c49ecb5d2555afc8befbe31998
Vincent <qianbo@futsystems.com>
2021-08-02 15:10:58 +0800

    add game fk

--- gamefi/models/account.py
+++ gamefi/models/account.py
@@ -3,16 +3,21 @@
 
 from django.db import models
 
+from .game import Game
 
 class Account(models.Model):
     """
     Game Account
     """
     account_id = models.CharField('ID', max_length=50, default='0')
+    game = models.ForeignKey(Game, related_name='accounts', verbose_name='Game',
+                                  on_delete=models.SET_NULL,
+                                  default=None,
+                                  blank=True, null=True)
     address = models.CharField('Address', max_length=100, default='')
+
     chain_token_balance = models.FloatField('Chain Token', default=0)
     game_token_balance = models.FloatField('Game Token', default=0)
-
     description = models.CharField('Description', max_length=1000, default='', blank=True)
 
     class Meta:
